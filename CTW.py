1b0298d7d65b1554e7cbeb9081e9989ca7f04aa7
matthp <matthp@rogers.com>
2016-01-04 22:37:54 -0500

    Format changes

--- CTW.py
+++ CTW.py
@@ -1,23 +1,22 @@
-__author__ = 'Matthew Howe-Patterson'
-
 import numpy as np
 from itertools import chain, izip
 
+
 class CTWNode:
     # Constructor
-    def __init__(self, parentIndex, depth):
+    def __init__(self, parent_index, depth):
         self.LogProbability = 0  # Stores the log probability of the data partition this node has seen so far
         self.OneCount = 0  # Stores the number of 1's of the data partition this node has seen so far
         self.ZeroCount = 0  # Stores the number of 0's of the data partition this node has seen so far
         self.OneChildIndex = -1  # The index of the child node when the context splits on 1
         self.ZeroChildIndex = -1 # The index of the child node when the context splits on 0
-        self.ParentNodeIndex = parentIndex  # The index of this nodes parent
+        self.ParentNodeIndex = parent_index  # The index of this nodes parent
         self.Depth = depth  # The depth in the tree of this node
         self.ChildRegister = []  # Child nodes register there log probabilities here during a recursive computation of
         # the log probability of the whole tree
 
-    # Prints the instance variables of the node
-    def printNode(self):
+    # Prints the instance variables of the node, used for debugging
+    def print_node(self):
         print '\nNew Node:'
         print 'LogProbability: ' + np.str(self.LogProbability)
         print 'OneCount: ' + np.str(self.OneCount)
@@ -29,23 +28,23 @@ class CTWNode:
         print 'ChildRegister: ' + np.str(self.ChildRegister)
 
     # Accessor for Depth property
-    def getDepth(self):
+    def get_depth(self):
         return self.Depth
 
     # Called by child nodes during the recursive computation of log probability
-    def registerLogProbability(self, logProbability):
-        self.ChildRegister.append(logProbability)
+    def register_log_probability(self, log_probability):
+        self.ChildRegister.append(log_probability)
 
     # Mutator for OneChildIndex property
-    def registerOneChild(self, oneChildIndex):
-        self.OneChildIndex = oneChildIndex
+    def register_one_child(self, one_child_index):
+        self.OneChildIndex = one_child_index
 
     # Mutator for ZeroChildIndex property
-    def registerZeroChild(self, zeroChildIndex):
-        self.ZeroChildIndex = zeroChildIndex
+    def register_zero_child(self, zero_child_index):
+        self.ZeroChildIndex = zero_child_index
 
     # Updates the log probability of the node and increments the one count. Returns the index of the next node.
-    def presentOneAndUpdate(self, cntxt):
+    def present_one_and_update(self, cntxt):
 
         # Update the nodes log probability using KT estimator
         self.LogProbability = self.LogProbability + np.log(self.OneCount + 0.5) - np.log(
@@ -55,15 +54,15 @@ class CTWNode:
         self.OneCount += 1
 
         # Return the index of the next child node given the context split
-        contextBit = cntxt[self.Depth]
+        context_bit = cntxt[self.Depth]
 
-        if contextBit == 1:
+        if context_bit == 1:
             return self.OneChildIndex
         else:
             return self.ZeroChildIndex
 
     # Updates the log probability of the node and increments the zero count. Returns the index of the next node.
-    def presentZeroAndUpdate(self, cntxt):
+    def present_zero_and_update(self, cntxt):
 
         # Update the nodes log probability using KT estimator
         self.LogProbability = self.LogProbability + np.log(self.ZeroCount + 0.5) - np.log(
@@ -73,120 +72,121 @@ class CTWNode:
         self.ZeroCount += 1
 
         # Return the index of the next child node given the context split
-        contextBit = cntxt[self.Depth]
+        context_bit = cntxt[self.Depth]
 
-        if contextBit == 1:
+        if context_bit == 1:
             return self.OneChildIndex
         else:
             return self.ZeroChildIndex
 
-    # Computes the log probability of the subtree under this node, after the children have registered there sub tree log probabilities.
+    # Computes the log probability of the subtree under this node, after the children have registered there sub tree
+    # log probabilities.
     # Returns the value of the log probability and the index of this nodes parent.
-    def computeLogProbability(self):
+    def compute_log_probability(self):
 
         # Average the probabilities of the child nodes and this node
-        passValue = np.logaddexp(np.log(0.5) + self.LogProbability, np.log(0.5) + sum(self.ChildRegister))
+        pass_value = np.logaddexp(np.log(0.5) + self.LogProbability, np.log(0.5) + sum(self.ChildRegister))
 
         # Reset the log probability register
         self.ChildRegister = []
 
         # Return the log probability of this nodes subtree and this nodes parent index for registering
-        return ([passValue, self.ParentNodeIndex])
+        return ([pass_value, self.ParentNodeIndex])
 
 
 class CTW:
 
     # Constructor
-    def __init__(self, maxDepth):
-        self.MaxDepth = maxDepth  # The context length that is used (maximum order of markov model)
+    def __init__(self, max_depth):
+        self.MaxDepth = max_depth  # The context length that is used (maximum order of markov model)
         self.NodeList = [CTWNode(-1, 0)]  # Stores the trees nodes that have been instantiated
 
     # Prints the instance variables of all the nodes of the tree
-    def printTree(self):
+    def print_tree(self):
         print 'MaxDepth: ' + np.str(self.MaxDepth)
         for x in self.NodeList:
-            x.printNode()
+            x.print_node()
 
     # Recursively compute the log probability of the entire tree
-    def computeLogProbability(self):
+    def compute_log_probability(self):
 
         # Starting with leaves, register each nodes log probability with it's parent
         for x in reversed(self.NodeList):
 
-            [passValue, parentNodeIndex] = x.computeLogProbability()
+            [pass_value, parent_node_index] = x.compute_log_probability()
 
-            if parentNodeIndex >= 0:
-                self.NodeList[parentNodeIndex].registerLogProbability(passValue)
+            if parent_node_index >= 0:
+                self.NodeList[parent_node_index].register_log_probability(pass_value)
 
-        return passValue
+        return pass_value
 
-    def presentBitStringAndUpdateWithSideInformation(self,bitString,sideInformation):
+    def present_bit_string_and_update_with_side_information(self, bit_string, side_information):
 
-        for index in enumerate(bitString, start = self.MaxDepth):
-            if index[0]+1 >= len(bitString):
+        for index in enumerate(bit_string, start=self.MaxDepth):
+            if index[0]+1 >= len(bit_string):
                 break
 
-            cntxt = list(chain.from_iterable(izip(bitString[index[0]-self.MaxDepth:index[0]], sideInformation[(index[0]+1)-self.MaxDepth:(index[0]+1)])))
+            cntxt = list(chain.from_iterable(izip(bit_string[index[0]-self.MaxDepth:index[0]], side_information[(index[0]+1)-self.MaxDepth:(index[0]+1)])))
 
-            if bitString[index[0]] == 1:
-                self.presentOneAndUpdateInContext(cntxt)
+            if bit_string[index[0]] == 1:
+                self.present_one_and_update_in_context(cntxt)
             else:
-                self.presentZeroAndUpdateInContext(cntxt)
+                self.present_zero_and_update_in_context(cntxt)
 
-    def presentBitStringAndUpdate(self,bitString):
-        for index in enumerate(bitString, start = self.MaxDepth):
-            if index[0]+1 >= len(bitString):
+    def present_bit_string_and_update(self, bit_string):
+        for index in enumerate(bit_string, start=self.MaxDepth):
+            if index[0]+1 >= len(bit_string):
                 break
-            if bitString[index[0]+1] == 1:
-                self.presentOneAndUpdateInContext(bitString[index[0]-self.MaxDepth:index[0]])
+            if bit_string[index[0]+1] == 1:
+                self.present_one_and_update_in_context(bit_string[index[0]-self.MaxDepth:index[0]])
             else:
-                self.presentZeroAndUpdateInContext(bitString[index[0]-self.MaxDepth:index[0]])
+                self.present_zero_and_update_in_context(bit_string[index[0]-self.MaxDepth:index[0]])
 
-    def presentOneAndUpdateInContext(self,cntxt):
-        tempcntxt = cntxt[::-1]
-        notDone = 1
+    def present_one_and_update_in_context(self, cntxt):
+        temp_cntxt = cntxt[::-1]
+        not_done = 1
         index = 0
         count = 0
-        while notDone == 1:
+        while not_done == 1:
 
             if count >= self.MaxDepth:
                 break
 
-            nextIndex = self.NodeList[index].presentOneAndUpdate(tempcntxt)
-            if nextIndex >= 0:
-                index = nextIndex
-            elif (nextIndex == -1) and (self.NodeList[index].getDepth() < self.MaxDepth):
+            next_index = self.NodeList[index].present_one_and_update(temp_cntxt)
+            if next_index >= 0:
+                index = next_index
+            elif (next_index == -1) and (self.NodeList[index].get_depth() < self.MaxDepth):
 
-                self.NodeList.append(CTWNode(index,self.NodeList[index].getDepth()+1))
-                if tempcntxt[count] == 1:
-                    self.NodeList[index].registerOneChild(len(self.NodeList)-1)
+                self.NodeList.append(CTWNode(index, self.NodeList[index].get_depth()+1))
+                if temp_cntxt[count] == 1:
+                    self.NodeList[index].register_one_child(len(self.NodeList)-1)
                 else:
-                    self.NodeList[index].registerZeroChild(len(self.NodeList)-1)
+                    self.NodeList[index].register_zero_child(len(self.NodeList)-1)
                 index = self.NodeList.__len__() - 1
             else:
-                notDone = 0
+                not_done = 0
             count += 1
 
-    def presentZeroAndUpdateInContext(self,cntxt):
-        tempcntxt = cntxt[::-1]
-        notDone = 1
+    def present_zero_and_update_in_context(self, cntxt):
+        temp_cntxt = cntxt[::-1]
+        not_done = 1
         index = 0
         count = 0
-        while notDone == 1:
+        while not_done == 1:
 
             if count >= self.MaxDepth:
                 break
 
-            nextIndex = self.NodeList[index].presentZeroAndUpdate(tempcntxt)
-            if nextIndex >= 0:
-                index = nextIndex
-            elif (nextIndex == -1) and (self.NodeList[index].getDepth() < self.MaxDepth):
-                self.NodeList.append(CTWNode(index,self.NodeList[index].getDepth()+1))
-                if tempcntxt[count] == 1:
-                    self.NodeList[index].registerOneChild(len(self.NodeList)-1)
+            next_index = self.NodeList[index].present_zero_and_update(temp_cntxt)
+            if next_index >= 0:
+                index = next_index
+            elif (next_index == -1) and (self.NodeList[index].get_depth() < self.MaxDepth):
+                self.NodeList.append(CTWNode(index,self.NodeList[index].get_depth()+1))
+                if temp_cntxt[count] == 1:
+                    self.NodeList[index].register_one_child(len(self.NodeList)-1)
                 else:
-                    self.NodeList[index].registerZeroChild(len(self.NodeList)-1)
+                    self.NodeList[index].register_zero_child(len(self.NodeList)-1)
                 index = self.NodeList.__len__() - 1
             else:
-                notDone = 0
+                not_done = 0
             count += 1
--- InformationDistance.py
+++ InformationDistance.py
@@ -1,36 +1,38 @@
-__author__ = 'Matthew Howe-Patterson'
-
 from CTW import CTW
 
-def estimateInfoDistance(X,Y, contextLength):
+# Estimates the information distance between x and y
+# x and y are arrays containing only ones or zeros (binary) and must be the same length
+
+
+def estimate_info_distance(x, y, context_length):
 
     # Return -1 if the lengths of the strings do not match
-    if len(X) != len(Y):
+    if len(x) != len(y):
         return -1
 
     # Estimate the entropy of X using CTW
-    Cx = CTW(contextLength)
-    Cx.presentBitStringAndUpdate(X)
-    Hx = -Cx.computeLogProbability() / len(X)
-    Cx = []
+    model_x = CTW(context_length)
+    model_x.present_bit_string_and_update(x)
+    entropy_x = -model_x.compute_log_probability() / len(x)
+    model_x = []
 
     # Estimate the entropy of Y using CTW
-    Cy = CTW(contextLength)
-    Cy.presentBitStringAndUpdate(Y)
-    Hy = -Cy.computeLogProbability() / len(Y)
-    Cy = []
+    model_y = CTW(context_length)
+    model_y.present_bit_string_and_update(y)
+    entropy_y = -model_y.compute_log_probability() / len(y)
+    model_y = []
 
     # Estimate the entropy of X conditioned on Y using CTW
-    Cxy = CTW(contextLength)
-    Cxy.presentBitStringAndUpdateWithSideInformation(X,Y)
-    Hxy = -Cxy.computeLogProbability() / len(X)
-    Cxy = []
+    model_xy = CTW(context_length)
+    model_xy.present_bit_string_and_update_with_side_information(x, y)
+    entropy_xy = -model_xy.compute_log_probability() / len(x)
+    model_xy = []
 
     # Estimate the entropy of Y conditioned on X using CTW
-    Cyx = CTW(contextLength)
-    Cyx.presentBitStringAndUpdateWithSideInformation(Y,X)
-    Hyx = -Cyx.computeLogProbability() / len(Y)
-    Cyx = []
+    model_yx = CTW(context_length)
+    model_yx.present_bit_string_and_update_with_side_information(y, x)
+    entropy_yx = -model_yx.compute_log_probability() / len(y)
+    model_yx = []
 
     # Compute the estimated normalized information distance between X and Y
-    return max([Hxy,Hyx]) / max([Hx, Hy])
+    return max([entropy_xy, entropy_yx]) / max([entropy_x, entropy_y])
